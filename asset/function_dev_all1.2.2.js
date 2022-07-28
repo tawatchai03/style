b14524ccbbaadeac9b034963dd3cac8253e57fad
mai_tawat <m.tawatchai03@gmail.com>
2022-07-28 19:01:12 +0700

    up vdo

--- asset/function_dev_all1.2.2.js
+++ asset/function_dev_all1.2.2.js
@@ -441,7 +441,55 @@
 	            }
 	        }
 
+	        /*function Youtube Official*/
 
+	        // 1. This code loads the IFrame Player API code asynchronously.
+	        var tag = document.createElement('script');
+
+	        tag.src = "https://www.youtube.com/iframe_api";
+	        var firstScriptTag = document.getElementsByTagName('script')[0];
+	        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	        // 2. This function creates an <iframe> (and YouTube player)
+	        //    after the API code downloads.
+	        var player;
+
+	        function onYouTubeIframeAPIReady() {
+	            player = new YT.Player('player', {
+	                height: '100%',
+	                width: '100%',
+	                videoId: YoutubeURL,
+	                playerVars: {
+	                    'playsinline': 1
+	                },
+	                events: {
+	                    'onReady': onPlayerReady,
+	                    'onStateChange': onPlayerStateChange
+	                }
+	            });
+	        }
+
+	        // 3. The API will call this function when the video player is ready.
+	        function onPlayerReady(event) {
+	            event.target.playVideo();
+	        }
+
+	        // 4. The API calls this function when the player's state changes.
+	        //    The function indicates that when playing a video (state=1),
+	        //    the player should play for six seconds and then stop.
+	        var done = false;
+
+	        function onPlayerStateChange(event) {
+	            if (event.data == YT.PlayerState.PLAYING && !done) {
+	                setTimeout(stopVideo, 6000);
+	                done = true;
+	            }
+	        }
+
+	        function stopVideo() {
+	            player.stopVideo();
+	        }
+	        /*End function Youtube Official*/
 
 	        //console.log( 'Register url : ' +SpinixRegisterURL );
 	        //console.log( 'Diamond frame url : ' +SpinixDiamondURL );
