1e8acdec162f3a09273a6cfe618300c0ef55dc71
mai_tawat <m.tawatchai03@gmail.com>
2022-07-29 00:45:04 +0700

    fff

--- asset/function_dev_all1.2.2.js
+++ asset/function_dev_all1.2.2.js
@@ -449,4 +449,46 @@
 
 	}, false);
 
+	/*function Youtube Official*/
+
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	var player;
+
+	function onYouTubeIframeAPIReady() {
+	    player = new YT.Player('player', {
+	        height: '100%',
+	        width: '100%',
+	        videoId: 'aPUVUrS2oC0',
+	        playerVars: {
+	            'playsinline': 1
+	        },
+	        events: {
+	            'onReady': onPlayerReady,
+	            'onStateChange': onPlayerStateChange
+	        }
+	    });
+	}
+
+	function onPlayerReady(event) {
+	    event.target.playVideo();
+	}
+
+	var done = false;
+
+	function onPlayerStateChange(event) {
+	    if (event.data == YT.PlayerState.PLAYING && !done) {
+	        setTimeout(stopVideo, 6000);
+	        done = true;
+	    }
+	}
+
+	function stopVideo() {
+	    player.stopVideo();
+	}
+	/*End function Youtube Official*/
+
 	/**---------------------------------------------------------------------*/
