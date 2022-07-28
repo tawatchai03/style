92dc249d8366a7c1a6e25156b9234c2a3e2cdd51
mai_tawat <m.tawatchai03@gmail.com>
2022-07-29 00:09:56 +0700

    up

--- asset/function_dev_all1.2.2.js
+++ asset/function_dev_all1.2.2.js
@@ -429,23 +429,7 @@
 	                console.log('ERROR: #' + SetFACEBOOKOfficialLink[j] + ' EL NOT EXIST.');
 	            }
 	        }
-	        /*function Youtube Official*/
-	        function validateYouTubeUrl() {
-	            var url = $('#youTubeUrl').val();
-	            if (url != undefined || url != '') {
-	                var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
-	                var match = url.match(regExp);
-	                if (match && match[2].length == 11) {
-	                    // Do anything for being valid
-	                    // if need to change the url to embed url then use below line            
-	                    $('#videoObject').attr('src', 'https://www.youtube.com/embed/' + match[2] + '?autoplay=1&enablejsapi=1');
-	                } else {
-	                    alert('not valid');
-	                    // Do anything for not being valid
-	                }
-	            }
-	        }
-	        /*End function Youtube Official*/
+
 
 	        //console.log( 'Register url : ' +SpinixRegisterURL );
 	        //console.log( 'Diamond frame url : ' +SpinixDiamondURL );
@@ -464,4 +448,22 @@
 
 
 	}, false);
+
+	/*function Youtube Official*/
+	function validateYouTubeUrl() {
+	    var url = $('#youTubeUrl').val();
+	    if (url != undefined || url != '') {
+	        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+	        var match = url.match(regExp);
+	        if (match && match[2].length == 11) {
+	            // Do anything for being valid
+	            // if need to change the url to embed url then use below line            
+	            $('#videoObject').attr('src', 'https://www.youtube.com/embed/' + match[2] + '?autoplay=1&enablejsapi=1');
+	        } else {
+	            alert('not valid');
+	            // Do anything for not being valid
+	        }
+	    }
+	}
+	/*End function Youtube Official*/
 	/**---------------------------------------------------------------------*/
