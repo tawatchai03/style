4dbd782738cec2c4b3950c83909349a8bcae184b
mai_tawat <m.tawatchai03@gmail.com>
2022-07-29 00:27:09 +0700

    sdf

--- asset/function_dev_all1.2.2.js
+++ asset/function_dev_all1.2.2.js
@@ -220,7 +220,7 @@
 	        var SpinixRegisterURL = BuildSpinixRegisterLink(ServerURL);
 	        var SiteLINEOfficial = BuildLINEOfficialLink(_siteconfig.setting.line_id);
 	        var SiteFACEBOOKOfficial = BuildFACEBOOKOfficialLink(_siteconfig.setting.facebookURL);
-	        var YoutubeURL = youTubeVideoIdFromUrl(_siteconfig.setting.youtubeURL);
+	        //var YoutubeURL = youTubeVideoIdFromUrl(_siteconfig.setting.youtubeURL);
 
 	        //var SetLoginElementLink = window.btnLoginIds;
 	        var SetRegisElementLink = window.registerIds;
