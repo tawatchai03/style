ccdc3ad88aacfb62f3a29b4f6cabc6bc189875a0
mai_tawat <m.tawatchai03@gmail.com>
2022-08-02 00:28:06 +0700

    you

--- asset/function_dev_allmai1.2.3.js
+++ asset/function_dev_allmai1.2.3.js
@@ -247,6 +247,46 @@
 	            document.getElementById("vdoplay").style.display = "none";
 	        }*/
 
+	        // Find all the YouTube video embedded on a page
+	        var videos = document.getElementsByClassName("youtube");
+
+	        for (var i = 0; i < videos.length; i++) {
+
+	            var youtube = videos[i];
+
+	            // Based on the YouTube ID, we can easily find the thumbnail image
+	            var img = document.createElement("img");
+	            img.setAttribute("src", "http://i.ytimg.com/vi/" +
+	                youtube.id + "/hqdefault.jpg");
+	            img.setAttribute("class", "thumb");
+
+
+	            // Overlay the Play icon to make it look like a video player
+	            var circle = document.createElement("div");
+	            circle.setAttribute("class", "circle");
+
+	            youtube.appendChild(img);
+	            youtube.appendChild(circle);
+
+	            // Attach an onclick event to the YouTube Thumbnail
+	            youtube.onclick = function() {
+
+	                // Create an iFrame with autoplay set to true
+	                var iframe = document.createElement("iframe");
+	                iframe.setAttribute("src",
+	                    "https://www.youtube.com/embed/" + YoutubeURL +
+	                    "?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1");
+
+	                // The height and width of the iFrame should be the same as parent
+	                iframe.style.width = this.style.width;
+	                iframe.style.height = this.style.height;
+
+	                // Replace the YouTube thumbnail with YouTube HTML5 Player
+	                this.parentNode.replaceChild(iframe, this);
+
+	            };
+	        }
+
 	        window.spinixIFrameUrlSource = SpinixDiamondURL
 	        window.spinixRegisterURLSource = SpinixRegisterURL
 
